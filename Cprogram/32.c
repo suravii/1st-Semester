1dc80ec591a614cf0a0cda5ac2959ef4a286458d
Suravi Shrestha <87415031+suravii@users.noreply.github.com>
2024-07-17 23:59:32 +0545

    Add files via upload

---
+++ Cprogram/32.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+void main() 
+{
+	int a,b,op, ch='y';
+	while(ch!='x')
+	{
+	printf("\nenter desired operator(+, -, *, / , % ) : ");
+	scanf(" %c",&op);
+	printf("enter any 2 number : ");
+	scanf("%d%d",&a,&b);
+
+	switch(op)
+    	{
+		
+		case '+':
+			 printf("addition : %d",a+b);
+			 break;
+		case '-':
+			 printf("subraction : %d", a-b);
+			 break;
+		case '*':
+			printf("Multiplication : %d", a*b);
+			 break;
+		 case '/':
+			printf("division : %d",a / b);
+			 break;
+		 case '%':
+			printf("modulus : %d",a % b);
+			 break;
+		default:
+			 printf("Error!You have entered an invalid choice");
+		 	 	 
+	 }
+		printf("\nPress y to conitnue\n Press x to exit \n ");
+		scanf(" %c",&ch);
+	}
+	
+}
